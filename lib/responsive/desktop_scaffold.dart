5f468344630888efd61aeb53d462145dcb21c345
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-22 18:02:09 +0600

    Desktop Responsive Ui Implemented

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveui/components/my_box.dart';
 import 'package:responsiveui/components/my_drawer.dart';
+import 'package:responsiveui/components/my_tile.dart';
 import 'package:responsiveui/theme/color_constants.dart';
 
 class DesktopScaffold extends StatefulWidget {
@@ -24,6 +26,44 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
           MyDrawer(),
 
           //rest of the things
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                //4 boxes on the top
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                          ),
+                      itemCount: 4,
+                      itemBuilder: (BuildContext context, int index) {
+                        return MyBox();
+                      },
+                    ),
+                  ),
+                ),
+
+                //tiles below it
+                Expanded(
+                  flex: 2,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return MyTile();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Expanded(child: Container(color: Colors.pink)),
+          ),
         ],
       ),
     );

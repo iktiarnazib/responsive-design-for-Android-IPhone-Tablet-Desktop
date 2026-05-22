b4dbdc50c5967fb8d162991070b8884f33b86b50
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-22 17:05:54 +0600

    Desktop drawer stays open when in desktop mode, + other ui improvements

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveui/components/my_drawer.dart';
+import 'package:responsiveui/theme/color_constants.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});
@@ -10,6 +12,20 @@ class DesktopScaffold extends StatefulWidget {
 class _DesktopScaffoldState extends State<DesktopScaffold> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.blue);
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: appbarColor,
+        iconTheme: IconThemeData(color: backgroundColor),
+      ),
+      body: Row(
+        children: [
+          //opened drawer
+          MyDrawer(),
+
+          //rest of the things
+        ],
+      ),
+    );
   }
 }
--- lib/responsive/tablet_scaffold.dart
+++ lib/responsive/tablet_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveui/components/my_drawer.dart';
+import 'package:responsiveui/theme/color_constants.dart';
 
 class TabletScaffold extends StatefulWidget {
   const TabletScaffold({super.key});
@@ -10,6 +12,13 @@ class TabletScaffold extends StatefulWidget {
 class _TabletScaffoldState extends State<TabletScaffold> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.pink);
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: appbarColor,
+        iconTheme: IconThemeData(color: backgroundColor),
+      ),
+      drawer: MyDrawer(),
+    );
   }
 }
